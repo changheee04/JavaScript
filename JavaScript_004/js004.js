d5e2ffeb2a6c744b48b50311822ea03f47558e1e
changheee04 <changheee04@gmail.com>
2023-05-23 00:21:37 +0900

    update 002, 003 / insert 004, 005, 006, 007

---
+++ JavaScript_004/js004.js
@@ -0,0 +1,6 @@
+const a = 10;
+const b = 20;
+const sum = 10 + 20;
+
+console.log(sum);
+console.log("안녕하세요. ", "지금은 ", new Date(), "입니다.");
---
+++ JavaScript_006/js006.js
@@ -0,0 +1,37 @@
+let myName = "김창희";
+alert(myName);
+
+let myString = "창희김";
+console.log(myString);
+let currentDate = new Date();
+console.log(currentDate);
+let myFunction = () => console.log("함수 확인");
+myFunction();
+console.log(myFunction);
+console.log(myFunction());
+
+let myName2 = "펭귄";
+myName2 = "고래";
+
+let number1 = 10;
+let number2 = 20;
+let sum = number1 + number2;
+console.log(sum);
+
+let firstName = "브레던";
+let lastName = "아이크";
+let fullName = firstName + lastName;
+console.log(fullName);
+
+let value1 = 100;
+let value2 = value1;
+console.log(value2);
+
+let value;
+console.log(value);
+
+let a = 1,
+  b = 2,
+  c;
+console.log(a + b);
+console.log(c);
